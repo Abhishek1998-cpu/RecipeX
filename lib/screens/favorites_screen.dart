c8c16c17790db66826207ce7c73855939852fe17
Abhishek <abhishekverma998@gmail.com>
2021-03-08 23:23:03 +0530

    New

---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,31 @@
+import '../models/meal.dart';
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+  FavoritesScreen(this.favoriteMeals);
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text("You don't have any favorites"),
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: favoriteMeals[index].id,
+            title: favoriteMeals[index].title,
+            imageUrl: favoriteMeals[index].imageUrl,
+            duration: favoriteMeals[index].duration,
+            affordability: favoriteMeals[index].affordability,
+            complexity: favoriteMeals[index].complexity,
+            // removeItem: _removeMeal,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+  }
+}
